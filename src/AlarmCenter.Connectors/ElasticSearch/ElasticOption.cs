2c4b7ee3c85dbd5a85274b109aaff6848003d2bf
Erik Xu <81636759@qq.com>
2018-11-26 00:33:48 +0800

    Add elastic search connector.

---
+++ src/AlarmCenter.Connectors/ElasticSearch/ElasticOption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace AlarmCenter.Connectors.ElasticSearch
+{
+    public class ElasticOption
+    {
+        public ElasticOption(IConfiguration config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            var section = config.GetSection("elastic");
+            section.Bind(this);
+        }
+
+        public string Uri { get; set; }
+
+        public string DefaultIndex { get; set; }
+    }
+}
---
+++ src/AlarmCenter.Connectors/ElasticSearch/ElasticServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+
+namespace AlarmCenter.Connectors.ElasticSearch
+{
+    public static class ElasticServiceCollectionExtensions
+    {
+        public static IServiceCollection AddElasticClient(this IServiceCollection services, IConfiguration config)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            services.AddOptions();
+
+            var elasticOption = new ElasticOption(config);
+
+            var settings = new ConnectionSettings(new Uri(elasticOption.Uri)).DefaultIndex(elasticOption.DefaultIndex);
+
+            services.Add(ServiceDescriptor.Singleton(new ElasticClient(settings)));
+            return services;
+        }
+    }
+}
